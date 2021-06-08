7c5b53ce46e94caa62141c72b3d30e6cc3a9b58a
DYY-Drummer <574861479@qq.com>
2021-06-08 14:34:31 +0800

    yes

---
+++ code-vue/vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? process.env.VUE_APP_BASE_URL
+    : '/',
+
+  configureWebpack: {
+    plugins: [
+    ],
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.common.js',
+        'jquery': 'jquery/src/jquery.js'
+      }
+    }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  },
+
+  chainWebpack: config => {
+    config.module.rule('pdf')
+      .test(/\.pdf$/)
+      .use('file-loader').loader('file-loader')
+  }
+}
